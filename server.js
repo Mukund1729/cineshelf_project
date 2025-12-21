1e4de5c012f8abaf516bee16f937de50f9e77a34
MUKUND <mukundmangal2173@gmail.com>
2025-12-21 12:58:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://cineshelf-project-mjde.vercel.app' // <<-- यह सही URL है
+  'https://visualcineaste.vercel.app' 
 ];
 
 app.use(cors({
