8bda8b8a73580635be8403307dd6a6a9c9afdd56
MUKUND <mukundmangal2173@gmail.com>
2025-12-22 16:19:46 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,4 @@
-================================================
-FILE: src/pages/Profile.jsx
-================================================
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
